aea5266b90f7b2132bb852132be24fc37c516c0a
Corey Trombley <corey.trombley33@gmail.com>
2013-04-25 09:40:01 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Elaineport::Application.config.secret_token = 'c60552a435886d11c55190c2cc29557ef15779b687cb1a502bc24b88b4e66e392191b359777f78b8433eabf6a1c638f1038269554a95690e14b1639041aa7a90'
